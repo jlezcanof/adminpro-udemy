deb1a9a63283682ab3b47dec865c606fe5e2951b
Lezcano <lezcano@192.168.1.10>
2020-01-02 12:37:27 +0100

    Input, output y viewchild

---
+++ src/app/components/grafico-dona/grafico-dona.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts';
+
+@Component({
+  selector: 'app-grafico-dona',
+  templateUrl: './grafico-dona.component.html',
+  styles: []
+})
+export class GraficoDonaComponent implements OnInit {
+
+  // Doughnut
+  @Input('chartLabels') doughnutChartLabels: Label[] = [];
+  @Input('chartData') doughnutChartData: MultiDataSet = [];
+  @Input('chartType') doughnutChartType: ChartType = null;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,19 +1,34 @@
+import { PAGES_ROUTES } from './pages.routes';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { FormsModule } from '@angular/forms';
+
 import { NgModule } from '@angular/core';
-import {PAGES_ROUTES} from './pages.routes';
+
+// Modulos
+
+// ng2-charts
+import { ChartsModule } from 'ng2-charts';
+
 import { PagesComponent } from './pages.component';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 
-// Modulos
-import { SharedModule } from '../shared/shared.module';
+// temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 @NgModule({
     declarations: [
      DashboardComponent,
      ProgressComponent,
      Graficas1Component,
-     PagesComponent
+     PagesComponent,
+     IncrementadorComponent,
+     GraficoDonaComponent
     ],
     exports: [
     DashboardComponent,
@@ -22,7 +37,9 @@ import { SharedModule } from '../shared/shared.module';
     ]
     , imports : [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
     ]
 })
 
